1d3fdb38c811876ebf2ab6f0c55cdcc5fefc047d
HolyTamEze <102805172+HolyTamEze@users.noreply.github.com>
2022-04-25 22:25:13 -0400

    Add files via upload

---
+++ Ever-Changing Maze.py
@@ -0,0 +1,20 @@
+import random
+
+def MazeCreator(rows):
+    wall = "W"
+    pieces = ["W", "S"]
+    
+    for i in range(1, rows + 1):
+        for j in range(i, rows + 1):
+            print(wall, end = ' ')
+        for k in range(1, i):
+            print(random.choice(pieces), end = ' ')
+        print()
+    return ""
+    
+    
+            
+    
+
+print(MazeCreator(int(input("Enter a number for the rows: "))))
+
